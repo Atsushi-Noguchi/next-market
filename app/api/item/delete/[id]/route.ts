9196b215920916f65c9c240fe0ba3d687c41a9c6
Atsushi-Noguchi <gunogunoatushi@gmail.com>
2023-12-10 21:57:55 +0900

    登録者本人以外の更新・削除を制御する機能の追加

--- app/api/item/delete/[id]/route.ts
+++ app/api/item/delete/[id]/route.ts
@@ -3,13 +3,21 @@ import connectDB from "@/app/utils/database";
 import { ItemModel } from "@/app/utils/schemaModels";
 
 export async function DELETE(request: NextRequest, context: NextFetchEvent) {
+  const reqBody = await request.json(); // リクエストボディから更新データを取得
   try {
     await connectDB();
     const id = request.nextUrl.pathname.split("/").pop();
-    await ItemModel.deleteOne({ _id: id });
-    return NextResponse.json({
-      message: "アイテムの削除に成功しました",
-    });
+    const currentItem = await ItemModel.findById(id); // 現在のアイテムを取得
+    if (currentItem.email !== reqBody.email) {
+      await ItemModel.deleteOne({ _id: id });
+      return NextResponse.json({
+        message: "アイテムの削除に成功しました",
+      });
+    } else {
+      return NextResponse.json({
+        message: "他のユーザーのアイテムは削除できません",
+      });
+    }
   } catch (err) {
     return NextResponse.json({ message: "アイテムの削除に失敗しました" });
   }
--- app/api/item/update/[id]/route.ts
+++ app/api/item/update/[id]/route.ts
@@ -3,13 +3,23 @@ import connectDB from "@/app/utils/database";
 import { ItemModel } from "@/app/utils/schemaModels";
 
 export async function PUT(request: NextRequest, context: NextFetchEvent) {
+  const updateData = await request.json(); // リクエストボディから更新データを取得
   try {
     await connectDB();
     const id = request.nextUrl.pathname.split("/").pop();
-    const updateData = await request.json(); // リクエストボディから更新データを取得
+    const currentItem = await ItemModel.findById(id); // 現在のアイテムを取得
+
+    if (currentItem.email !== updateData.email) {
+      // 現在のメールアドレスと更新データのメールアドレスが一致しない場合、エラーを返す
+      return NextResponse.json({
+        message: "メールアドレスが一致しないため、更新できません",
+      });
+    }
+
     const updatedItem = await ItemModel.findByIdAndUpdate(id, updateData, {
       new: true,
     }); // アイテムを更新
+
     return NextResponse.json({
       message: "アイテム編集成功",
       data: updatedItem,
