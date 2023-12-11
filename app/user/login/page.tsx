d1db330295d6fddbda2e018ba06636bab8a998b1
Atsushi-Noguchi <gunogunoatushi@gmail.com>
2023-12-11 14:17:41 +0900

    ログインページの作成（デザインは後）

--- app/user/login/page.tsx
+++ app/user/login/page.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 const LoginPage: React.FC = () => {
-  return <div>{/* コンポーネントの内容をここに追加 */}</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // フォームの送信処理をここに追加
+    try {
+      const response = await fetch("http://localhost:3000/api/user/login", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const jsonData = await response.json();
+      localStorage.setItem("token", jsonData.token);
+      alert(jsonData.message);
+      // レスポンスの処理をここに追加
+    } catch (error) {
+      alert("ログインに失敗しました");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          placeholder="メールアドレス"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="パスワード"
+        />
+        <button type="submit">ログイン</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
