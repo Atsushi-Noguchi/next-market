3eebc1ffa2a91d9e4417909546aac9c7da9b6aff
Atsushi-Noguchi <gunogunoatushi@gmail.com>
2023-12-11 14:18:04 +0900

    全てのアイテムを表示するページを作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-const ReadAllItems = () => {
+import React from "react";
+import Image from "next/image";
+
+const getAllitems = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/item/readall", {
+      cache: "no-store",
+    });
+    const jsonData = await response.json();
+    const allItems = jsonData.data;
+    console.log(jsonData);
+    return allItems;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const ReadAllItems = async () => {
+  const allItems = await getAllitems();
   return (
     <div>
       <h1>こんにちは</h1>
+      {allItems.map((item: any) => (
+        <div key={item._id}>
+          <Image src={item.image} alt={item.title} width={500} height={300} />
+          <h2>{item.price}</h2>
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+        </div>
+      ))}
     </div>
   );
 };
